49b16d4650dd4d5090940339086c546199f8c9dc
BLACKMouha <mohamedsenghor05@gmail.com>
2023-10-05 14:11:23 +0000

    Create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const { getStats, getStatus } = require('../controllers/AppController');
+const { default: postNew } = require('../controllers/UsersController');
 
 const linkRoutes = (server) => {
   server.get('/status', getStatus);
   server.get('/stats', getStats);
+  server.post('/users', postNew);
 };
 
 module.exports = linkRoutes;
