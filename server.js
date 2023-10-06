83fd1498198e90a9deed01b13f39da86598712ed
BLACKMouha <mohamedsenghor05@gmail.com>
2023-10-06 17:28:16 +0000

    Enable req.body

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const linkRoutes = require('./routes');
 
 const port = process.env.PORT || 5000;
 const server = express();
+
+server.use(express.json());
+
 linkRoutes(server);
+
 server.listen(port, () => console.log(`Server is runnning on port ${port}`));
 
 module.exports = server;
